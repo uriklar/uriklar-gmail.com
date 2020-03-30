f1f6ff2b42397d35979529f414251edc47c6dee0
Uri Klar <uriklar@gmail.com>
2020-03-30 08:47:32 +0300

    dev server working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 export default function App() {
-  return <div>Hello world!</div>;
+  return <div>Hello wohhhrld!</div>;
 }
+
+ReactDOM.render(<App />, document.getElementById("app"));
