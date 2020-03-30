53ef0b7306e176e00608de2e782d26da3adc3ec1
Uri Klar <uriklar@gmail.com>
2020-03-30 08:59:37 +0300

    eslint done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 export default function App() {
-  const unused = 3;
-  return <div>Hello wohhhrld!</div>;
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    if (count === 3) {
+      alert("count equals 3!");
+    }
+  }, [count]);
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase count</button>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
